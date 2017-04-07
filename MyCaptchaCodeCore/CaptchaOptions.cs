ecdc4d84f3e5d63787881d6deb2d36460e20748d
moonlightttt <595241427@qq.com>
2017-04-07 14:24:31 +0800

    提交文件

---
+++ MyCaptchaCodeCore/CaptchaOptions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace MyCaptchaCodeCore
+{
+    public class CaptchaOptions
+    {
+        #region 私有字段
+
+        private int _width;
+        private int _height;
+        private int _length;
+        private string _chars;
+
+        #endregion
+
+        #region 公共属性
+
+        /// <summary>
+        /// 验证字符长度（字符个数）
+        /// </summary>
+        public int TextLength {
+            get { return _length; }
+            set {_length = value < 3 ? 3 : value;}
+        }
+
+        /// <summary>
+        /// 生成验证码用的字符
+        /// </summary>
+        public string TextChars {
+            get {return _chars;}
+            set {_chars =(string.IsNullOrEmpty(value)||value.Trim().Length < 3) ? "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" : value.Trim();}
+        }
+
+        /// <summary>
+        /// Font warp factor
+        /// </summary>
+        public Level FontWarp { get; set; }
+
+        /// <summary>
+        /// Background Noise level
+        /// </summary>
+        public Level BackgroundNoise { get; set; }
+
+        /// <summary>
+        /// 线条杂色级别
+        /// </summary>
+        public Level LineNoise { get; set; }
+
+
+        /// <summary>
+        /// 验证码图片宽度
+        /// </summary>
+        public int Width
+        {
+            get { return _width; }
+            set{_width = value < (TextLength * 18)?TextLength*18:value;}
+        }
+
+        /// <summary>
+        /// 验证码图片高度
+        /// </summary>
+        public int Height
+        {
+            get { return _height; }
+            set
+            {
+                _height = value<20?20:value;
+            }
+        }
+
+        #endregion
+
+        #region constructor
+
+        public CaptchaOptions()
+        {
+            FontWarp = Level.None;
+            BackgroundNoise = Level.Low;
+            LineNoise = Level.Low;
+            Width = 72;
+            Height = 22;
+            TextLength = 5;
+        }
+
+        #endregion
+    }
+
+    public enum Level
+    {
+        None,
+        Low,
+        Medium,
+        High,
+        Extreme
+    }
+}
